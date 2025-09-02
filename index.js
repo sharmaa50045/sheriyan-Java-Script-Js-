5c5c5edd1dabb20eb7025fbd53bb3dcae2ef01b9
Aaditya sharma <sharmaa50045@gmail.com>
2025-09-02 13:26:10 +0530

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
 // Arithmetic, Comparison, logical, assignment, unary, ternary
 
 // + - / * % ** // arithmetic operators
+
+
+// comparison operator //
+// =  
+// ==  not strict comparison
+// === strict
+// != not strict
+// !== strict
+// >= 
+// <= 
+// < 
+// > 
+
+// assigment //
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+
+//  assignment operator
+let a = 12;
+
+a += 3,
+  a -= 11;
+a *= 2;
+a /= 2;
+a %= 3;
+
+
+
+
+
+
+
+
+
+12 == 13 // false
+12 == "12" // true
+
+12 != 13 // true
+"harsh" != "harshita" // true
+
+12 != "12" // false
+12 !== "12" // true
+
+
+12 > 11 // true
+12 > 113 // false
+12 < 112 // true
+112 < 1145// false
